dd76781603e385c4e2463c6733222b8f4922b8f4
kaeuloze <mariskaesapurnomo@gmail.com>
2026-03-02 22:31:44 +0700

    install breeze

---
+++ resources/views/about.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            About Me
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white shadow-sm sm:rounded-lg p-6">
+                <p><strong>Nama:</strong> Mariska Esa Purnomo</p>
+                <p><strong>NIM:</strong> 20230140167</p>
+                <p><strong>Program Studi:</strong> Teknologi Informasi</p>
+                <p><strong>Hobi:</strong> Ngoding</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
